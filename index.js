091826988e3b52453e37637175c6c21a7af2ca59
Colten Rouska <crouska@gmail.com>
2021-06-16 16:30:36 -0600

    added uuid/v1 and filtering

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const cuid = require('cuid');
 const { nanoid } = require('nanoid');
 
 const ids = [
+  { title: 'uuid/v1', action: () => uuid.v1() },
   { title: 'uuid/v4', action: () => uuid.v4() },
   { title: 'cuid', action: () => cuid() },
   { title: 'shortId', action: () => shortid() },
   { title: 'nanoId', action: () => nanoid() },
 ];
 
+const actions = alfy.inputMatches(ids, 'title') || ids;
+
 alfy.output(
-  ids.map((res) => {
+  actions.map((res) => {
     const id = res.action();
 
     return { title: res.title, subtitle: id, arg: id };
